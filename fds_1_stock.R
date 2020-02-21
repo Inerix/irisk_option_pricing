490ae369975a7d42fb830cf7bee965858c948023
Inerix <jjlu2@illinois.edu>
2020-02-21 11:03:18 -0600

    Partially updated, still not working

--- fds_1_stock.R
+++ fds_1_stock.R
@@ -2,20 +2,34 @@
 # @ Params
 # r - risk free interest rate
 # time - Vector representing time slices
-# strike - Strike price of the stock (w2 * s2) - K
-# b - Vector of all possible stock prices (w1 * s1)
+# strike - Strike price of the stock : K -  (w2 * s2) 
+# b - Vector of all possible stock prices : (w1 * s1)
 # sigma - Volatility (Black-Scholes)
 #
 # @ Return - 2-d matrix filled using FDS
 
-fds_1s = function(r, time, strike, b, sigma, dt, db) {
+######
+# Testing Parameters
+# r = .01
+# T = 1
+# delta_t = T/nstep_time
+# time = seq(0, T, delta_t)
+# strike = K - weights[2] * b_2[1]
+# b = weights[1] * b_1
+# sigma = sigma[1]
+######
+
+
+fds_1s = function(r, time, strike, b, sigma) {
     l_t = length(time)
+    dt = time[2] - time[1]
+    db = b[2] - b[1]
     
     V_c = array(rep(0, (length(b)) * (l_t)), dim = c(l_t, length(b)))
     # [time, price]
 
     # First, consider final condition
-    V_c[1, ] = max(b - strike, 0)
+    V_c[1, ] = pmax(b - strike, 0)
     
     # Then, consider boundary condition
     V_c[, 1] = max(b[1] - strike, 0)
